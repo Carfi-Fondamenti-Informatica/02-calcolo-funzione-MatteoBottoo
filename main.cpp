d71e000c38a556fadaa33cddb25e2efac95f82ab
MatteoBottoo <115472031+MatteoBottoo@users.noreply.github.com>
2022-11-17 13:34:13 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,27 @@
 using namespace std;
 
 int main() {
+   
+   float a=0;
+   float b=0;
+   float x=0;
+   float z=0;
+   
+   cin >> a;
+   cin >> b;
+   cin >> x;
+   cin >> y;
+   cin >> z;
+   
+   if (x<0 && y>0){
+      z=a*x-b*y;
+   }else if (x>=0 && y<=0){
+      z=a*x*x-b*y;
+   }else{
+      z=a*x+b*y*y;
+   }
+   
+   cout << z;
+   
    return 0;
 }
